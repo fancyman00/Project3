8b7c231d92719ceec7bf5d2519819bbd8b07cd75
fancyman00 <123255462+fancyman00@users.noreply.github.com>
2024-12-06 01:36:21 +0300

    Update main.cpp

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -11,33 +11,37 @@
 #include <Windows.h>;
 #include <fstream>
 #include <limits>
-
 import Binary;
+
 void print_menu() {
-    std::cout << "\n1. Ввести двоичное число A с клавиатуры\n"
-        << "2. Ввести десятичное число A с клавиатуры\n"
-        << "3. Ввести двоичное число A из файла\n"
-        << "4. Вывести двоичное число A на экран\n"
-        << "5. Вывести десятичное число A на экран\n"
-        << "6. Сохранить двоичное число A в файл\n"
-        << "7. Выполнить операцию  A = A + B\n"
-        << "8. Выполнить операцию A = A - B\n"
-        << "9. Выполнить операцию A = A * B\n"
-        << "10. Выполнить операцию A = A / B\n"
-        << "11. Выполнить операцию A == B\n"
-        << "12. Выполнить операцию A <=> B\n"
-        << "13. Выполнить операцию A != B\n"
-        << "14. Выполнить операцию A < B\n"
-        << "15. Выполнить операцию A > B\n"
-        << "16. Выполнить операцию A <= B\n"
-        << "17. Выполнить операцию A >= B\n"
-        << "18. Выход\n"
-        << "Выберите действие: ";
+    std::cout << "\nМеню\n\n"
+        << "1. Ввести двоичное число с клавиатуры\n"
+        << "2. Ввести десятичное число с клавиатуры\n\n"
+        << "3. Ввести двоичное число из файла\n"
+        << "4. Сохранить двоичное число в файл\n\n"
+        << "5. Вывести значение переменной на экран\n\n"
+        << "6. Выполнить операцию  A + B\n"
+        << "7. Выполнить операцию A - B\n"
+        << "8. Выполнить операцию A * B\n"
+        << "9. Выполнить операцию A / B\n\n"
+        << "10. Выполнить операцию A == B\n"
+        << "11. Выполнить операцию A <=> B\n"
+        << "12. Выполнить операцию A != B\n"
+        << "13. Выполнить операцию A < B\n"
+        << "14. Выполнить операцию A > B\n"
+        << "15. Выполнить операцию A <= B\n"
+        << "16. Выполнить операцию A >= B\n\n"
+        << "17. Выход\n\n";
 }
-unsigned int input_size() {
+void print_sub_menu() {
+    std::cout << "\nВыберите переменную\n"
+        << "1. A\n"
+        << "2. B\n";
+}
+unsigned int input_uint(const char* message) {
     unsigned int size;
     while (true) {
-        std::cout << "Введите размер -> ";
+        std::cout << message;
         if (!(std::cin >> size)) {
             std::cout << "Ошибка: введите целое число." << std::endl;
             std::cin.clear();
@@ -48,8 +52,18 @@ unsigned int input_size() {
     }
     return size;
 }
+
+unsigned int input_variable() {
+    unsigned short choice = 0;
+    print_sub_menu();
+    while (choice != 1 && choice != 2) {
+        choice = input_uint("Выберите переменную -> ");
+    }
+    return choice;
+}
+
 Binary input_binary() {
-    unsigned int size = input_size();
+    unsigned int size = input_uint("Введите размер -> ");
     std::string binary_x;
 
     std::cout << "Введите двоичное число -> ";
@@ -59,8 +73,9 @@ Binary input_binary() {
     binary.from_binary_string(binary_x);
     return binary;
 }
+
 Binary input_decimal() {
-    unsigned int size = input_size();
+    unsigned int size = input_uint("Введите размер -> ");
     int x;
 
     while (true) {
@@ -78,10 +93,12 @@ Binary input_decimal() {
     binary.from_decimal(x);
     return binary;
 }
+
 int main() {
     setlocale(LC_ALL, "Russian");
-    Binary binary;
-    int decimal_x, size;
+    Binary binary_a;
+    Binary binary_b;
+    Binary res;
     std::string binary_x, filename;
     bool exit = false;
     int choice;
@@ -90,69 +107,107 @@ int main() {
     while (!exit) {
         try {
             print_menu();
-            std::cin >> choice;
+            choice = input_uint("Выберите действие -> ");
 
             switch (choice) {
             case 1:
-                binary = input_binary();
+                switch (input_variable()) {
+                    case 1:
+                        binary_a = input_binary();
+                        break;
+                    case 2:
+                        binary_b = input_binary();
+                        break;
+                }
                 break;
             case 2:
-                binary = input_decimal();
+                switch (input_variable()) {
+                case 1:
+                    binary_a = input_binary();
+                    break;
+                case 2:
+                    binary_b = input_binary();
+                    break;
+                }
                 break;
             case 3:
-                std::cout << "Введите название файлы -> ";
+                std::cout << "Введите название файла -> ";
                 std::cin >> filename;
                 in_file.open(filename);
                 if (in_file.is_open())
                 {
                     in_file >> binary_x;
                     in_file.close();
-                    binary = Binary(binary_x.length());
-                    binary.from_binary_string(binary_x);
+                    switch (input_variable()) {
+                    case 1:
+                        binary_a = Binary(binary_x.length());
+                        binary_a.from_binary_string(binary_x);
+                        break;
+                    case 2:
+                        binary_b = Binary(binary_x.length());
+                        binary_b.from_binary_string(binary_x);
+                        break;
+                    }
                 }
-                else 
-                {
-                    std::cout << "Не удалось открыть файл." << std::endl;
-                }
+                else
+                    throw std::runtime_error("Не удалось открыть файл.");
                 break;
             case 4:
-                std::cout << "Текущее число (Двоичный вид): " << binary.to_binary_string() << std::endl;
-                break;
-            case 5:
-                std::cout << "Текущее число (Десятичный вид): " << binary.to_decimal() << std::endl;
-                break;
-            case 6:
                 std::cout << "Введите название файла -> ";
                 std::cin >> filename;
                 out_file.open(filename);
-                if (out_file.is_open()) 
+                if (out_file.is_open())
                 {
-                    out_file << binary.to_binary_string() << std::endl;
+                    switch (input_variable()) {
+                    case 1:
+                        binary_a.empty_error();
+                        out_file << binary_a.to_binary_string() << std::endl;
+                        break;
+                    case 2:
+                        binary_b.empty_error();
+                        out_file << binary_b.to_binary_string() << std::endl;
+                        break;
+                    }
                     out_file.close();
                 }
-                else 
-                {
-                    std::cout << "Не удалось открыть файл." << std::endl;
+                else
+                    throw std::runtime_error("Не удалось открыть файл.");
+                break;
+            case 5:
+                switch (input_variable()) {
+                case 1:
+                    binary_a.empty_error();
+                    std::cout << "Переменная A - Двоичный вид: " << binary_a.to_binary_string() << " Десятичный вид: " << binary_a.to_decimal() << std::endl;
+                    break;
+                case 2:
+                    binary_b.empty_error();
+                    std::cout << "Переменная B - Двоичный вид: " << binary_b.to_binary_string() << " Десятичный вид: " << binary_b.to_decimal() << std::endl;
+                    break;
                 }
+                break;     
+            case 6:
+                res = binary_a + binary_b;
+                std::cout << "Двоичный вид: " << res.to_binary_string() << " Десятичный вид: " << res.to_decimal() << std::endl;
                 break;
             case 7:
-                binary = binary + input_binary();
+                res = binary_a - binary_b;
+                std::cout << "Двоичный вид: " << res.to_binary_string() << " Десятичный вид: " << res.to_decimal() << std::endl;      
                 break;
             case 8:
-                binary = binary - input_binary();
+                res = binary_a * binary_b;
+                std::cout << "Двоичный вид: " << res.to_binary_string() << " Десятичный вид: " << res.to_decimal() << std::endl;
                 break;
             case 9:
-                binary = binary * input_binary();
+                res = binary_a / binary_b;
+                std::cout << "Двоичный вид: " << res.to_binary_string() << " Десятичный вид: " << res.to_decimal() << std::endl;
                 break;
             case 10:
-                binary = binary / input_binary();
+                std::cout << (binary_a == binary_b ? "True" : "False") << std::endl;
                 break;
             case 11:
-                std::cout << (binary == input_binary() ? "True" : "False") << std::endl;
-                break;
-            case 12:
                 std::cout << std::endl;
-                std::strong_ordering bin = binary <=> input_binary();
+                std::strong_ordering bin = binary_a <=> binary_b;
+                std::cout << "Результат операции: ";
                 if (bin == std::strong_ordering::equal)
                     std::cout << "==" << std::endl;
                 else if (bin == std::strong_ordering::greater)
@@ -160,26 +215,26 @@ int main() {
                 else if (bin == std::strong_ordering::less)
                     std::cout << "<" << std::endl;
                 break;
+            case 12:
+                std::cout << (binary_a != binary_b ? "True" : "False") << std::endl;
+                break;
             case 13:
-                std::cout << (binary != input_binary() ? "True" : "False") << std::endl;
+                std::cout << (binary_a < binary_b ? "True" : "False") << std::endl;
                 break;
             case 14:
-                std::cout << (binary < input_binary() ? "True" : "False") << std::endl;
+                std::cout << (binary_a > binary_b ? "True" : "False") << std::endl;
                 break;
             case 15:
-                std::cout << (binary > input_binary() ? "True" : "False") << std::endl;
+                std::cout << (binary_a <= binary_b ? "True" : "False") << std::endl;
                 break;
             case 16:
-                std::cout << (binary <= input_binary() ? "True" : "False") << std::endl;
+                std::cout << (binary_a >= binary_b ? "True" : "False") << std::endl;
                 break;
             case 17:
-                std::cout << (binary >= input_binary() ? "True" : "False") << std::endl;
-                break;
-            case 18:
                 exit = true;
                 break;
             default:
-                std::cout << "Некорректный выбор, попробуйте снова.\n";
+                throw std::invalid_argument("Некорректный выбор, попробуйте снова.");
             }
         }
         catch (std::exception e) {
